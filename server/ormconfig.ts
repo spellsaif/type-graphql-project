0592a45e0798762999de714547378158cea3c422
Md Saif Ali Ansari <71248588+spellsaif@users.noreply.github.com>
2023-08-07 23:14:33 +0000

    fixed: database error

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -4,7 +4,7 @@ import { User } from "./src/modules/user/user.schema";
 
 export const dbConfig: DataSourceOptions = {
     type: "postgres",
-    url: "postgres://spellsaif:Q9E1oqBFCcxI@ep-icy-lake-74849568.ap-southeast-1.aws.neon.tech/neondb",
+    url: "postgres://spellsaif:Q9E1oqBFCcxI@ep-icy-lake-74849568.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
     logging: true,
     synchronize: true,
     entities: [User],
