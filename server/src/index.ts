e562d76bd101996df54127667705b3ad52e48e64
Md Saif Ali Ansari <71248588+spellsaif@users.noreply.github.com>
2023-08-07 21:42:10 +0000

    refactoring code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,18 @@
 import "reflect-metadata";
-import express from "express"
+import express, { Request } from "express"
 import { buildSchema } from "type-graphql";
 import { UserResolver } from "./modules/user/user.resolver";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4"
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer"
 import { json } from "body-parser";
 import cors from "cors";
 import config from 'config';
+import http from 'node:http'
+
+export type MyContext = {
+    req: Request
+}
 
 async function main() {
     const schema = await buildSchema({
@@ -15,18 +21,24 @@ async function main() {
 
     const app = express();
 
-    const server = new ApolloServer({
-        schema
+    const httpServer = http.createServer(app);
+
+    const server = new ApolloServer<MyContext>({
+        schema,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
     })
 
     await server.start();
 
-    app.use('/graphql', cors<cors.CorsRequest>(), json(), expressMiddleware(server));
+    app.use('/graphql', cors<cors.CorsRequest>(), json(), expressMiddleware(server, {
+        context: async ({ req }) => ({ req })
+    }));
 
     const SERVER_PORT = config.get('server.port');
-    app.listen(SERVER_PORT, () => {
-        console.log(`Listening to http://localhost:${SERVER_PORT}`);
-    })
+
+    await new Promise<void>((resolve) => httpServer.listen({ port: SERVER_PORT }, resolve));
+
+    console.log(`🚀 Server ready at http://localhost:${SERVER_PORT}/graphql`);
 
 }
 
